69aab6bd98e1d8d520177aa0ada759bdba65fc6f
Isha Pal <ishapal2004@gmail.com>
2026-06-17 19:59:33 +0530

    Added real-time alert logging and dashboard alerts panel

--- frontend/src/components/AlertPanel.jsx
+++ frontend/src/components/AlertPanel.jsx
@@ -1,22 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function AlertPanel() {
 
-  const alerts = [
-    {
-      title: "Drowsiness Detected",
-      time: "12:32 PM",
-      color: "bg-red-500"
-    },
-    {
-      title: "Yawning Detected",
-      time: "11:47 AM",
-      color: "bg-yellow-500"
-    },
-    {
-      title: "Back To Alert State",
-      time: "11:45 AM",
-      color: "bg-green-500"
-    }
-  ];
+  const [alerts, setAlerts] = useState([]);
+
+  useEffect(() => {
+
+    const fetchAlerts = () => {
+
+      axios
+        .get("http://127.0.0.1:5000/alerts")
+        .then((res) => {
+          setAlerts(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+    };
+
+    fetchAlerts();
+
+    const interval = setInterval(
+      fetchAlerts,
+      2000
+    );
+
+    return () => clearInterval(interval);
+
+  }, []);
 
   return (
     <div className="bg-slate-900 border border-slate-800 rounded-3xl p-6">
@@ -27,31 +40,29 @@ function AlertPanel() {
           Recent Alerts
         </h2>
 
-        <button className="bg-slate-800 px-4 py-2 rounded-lg">
-          View All
-        </button>
-
       </div>
 
       <div className="space-y-4">
 
-        {alerts.map((alert, index) => (
+        {alerts.length === 0 ? (
 
-          <div
-            key={index}
-            className="bg-slate-800 p-4 rounded-2xl flex justify-between"
-          >
+          <p className="text-slate-400">
+            No Alerts Yet
+          </p>
 
-            <div className="flex gap-4">
+        ) : (
 
-              <div
-                className={`w-12 h-12 rounded-full ${alert.color}`}
-              />
+          alerts.map((alert, index) => (
+
+            <div
+              key={index}
+              className="bg-slate-800 p-4 rounded-2xl flex justify-between"
+            >
 
               <div>
 
-                <h3 className="font-semibold">
-                  {alert.title}
+                <h3 className="font-semibold text-red-400">
+                  {alert.event}
                 </h3>
 
                 <p className="text-slate-400 text-sm">
@@ -60,15 +71,15 @@ function AlertPanel() {
 
               </div>
 
+              <span className="text-slate-400">
+                {alert.time}
+              </span>
+
             </div>
 
-            <span className="text-slate-400">
-              {alert.time}
-            </span>
+          ))
 
-          </div>
-
-        ))}
+        )}
 
       </div>
 
